04a006f4b1c13fc8dce505f1886e1329d77427d5
GonzaEnrique27 <gonz_1994@hotmail.com>
2022-05-25 12:58:57 -0300

    contador

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {useState} from 'react'
+import '../itemListContainer.css'
 
-function ItemListContainer ({titulo})  {
-  return (
-    <div>{titulo}</div>
-  )
-}
+function ItemListContainer ()  {
 
-export default ItemListContainer
+   
+   const [counter, setCounter] = useState(0)
+  
+   
+   const handleClick1 = () => {
+     setCounter(counter + 1)
+   }
+    
+   const handleClick2 = () => {
+   setCounter(counter - 1)
+   }
+   
+   return (
+     <div className='cnt-buttons'>
+       <div className='ctn-fbuttons'>
+         {counter}
+       </div>
+       <div className="buttons">
+       <button className='decrement' onClick={handleClick2}>-</button>
+       <button className='increment' onClick={handleClick1}>+</button> 
+       </div>
+     </div>
+   )
+ }
+   
+ export default ItemListContainer
