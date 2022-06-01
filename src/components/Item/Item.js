f935857c7a594b5eada5288209e1761b8af192d6
GonzaEnrique27 <gonz_1994@hotmail.com>
2022-06-01 02:55:52 -0300

    tarjetas

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -13,7 +13,6 @@ const Item = ({item}) => {
               <h3>{item.marca} {item.model} </h3>
               <h2 className="price">${item.price}</h2>
               <a href="#" className="buy">comprar ahora</a>
-              <a href="#" className="buy">detalle</a>
           </div> 
         </div>
         </div>
